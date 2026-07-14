a14682d6fbe6a9916f31817a4a5d677962ebfc70
amanchauhan1015 <a4648919@gmail.com>
2026-07-14 21:21:37 +0530

    Fix Render port

--- main.py
+++ main.py
@@ -119,5 +119,8 @@ def index():
         results.sort(key=lambda x: x[1], reverse=True)
 
     return render_template("index.html", results=results)
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
